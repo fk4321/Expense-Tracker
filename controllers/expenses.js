0fd01a23b009044850c03fdb9d8be68f4e8f8f23
fk4321 <139650641+fk4321@users.noreply.github.com>
2024-02-29 10:50:55 -0500

    fixed fetching expenses method and added validations to adding expenses method

--- controllers/expenses.js
+++ controllers/expenses.js
@@ -1,4 +1,5 @@
 const ExpenseSchema = require('../models/ExpenseModel')
+
 // add expense
 const addExpense = async (req, res) => {
    const { title, date, amount, category, description } = req.body
@@ -11,22 +12,31 @@ const addExpense = async (req, res) => {
     description
    })
    try {
+        //validations 
+        if ( !title || !date || !category || !description ) {
+            return res.status(400).json({message: "All fields are required"})
+        }
+        if ( amount <= 0 || !amount === "number" ) {
+            return res.status(400).json({message: "Enter a positive amount and Make sure it is a number"})
+        }
        expense.save() 
        res.status(200).json({message: "Expense Added"})
    } catch (error) {
         res.status(500).json({message: "Server Error"})
    }
 }
+
 // get expense
 const getExpense = async(req, res) => {
     try {
         const expense = await ExpenseSchema.find().sort({createdAt: -1})
-        req.send(expense)
+        res.status(200).json({expense})
     } catch (error) {
-        
+        res.status(500).json({message: "Server Error"})
     }
     
 }
+
 // delete expense 
 const deleteExpense = async (req,res) => {
     console.log(req.params)
@@ -39,6 +49,7 @@ const deleteExpense = async (req,res) => {
             res.status(500).json({message: 'Server Error'})
         }) 
 }
+
 module.exports = {
     addExpense, getExpense, deleteExpense
 }
